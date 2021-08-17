750529538daeb9ea48df5d2c961df72e8697b576
Barry Martin <bvmcode@gmail.com>
2021-08-16 22:13:34 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from dotenv import load_dotenv
+import yagmail
+
+load_dotenv()
+USERNAME = os.environ.get("GMAIL_USER")
+PASSWORD = os.environ.get("GMAIL_PWD")
+URL = "https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149"
+
+
+def email():
+    yag = yagmail.SMTP(USERNAME, PASSWORD)
+    yag.send(USERNAME, "PS5 Available - Best Buy", URL)
+
+
+def get_status():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-gpu")
+    driver = webdriver.Chrome(chrome_options=options)
+    driver.get(URL)
+    contents = driver.find_element_by_class_name("add-to-cart-button")
+    if contents.text != "Sold Out":
+        email()
+
+    driver.close()
+
+
+if __name__ == "__main__":
+    get_status()
